2e9dd5464524e3f46722852ddeffa2e949f71783
Osmar238 <osmar77770@gmail.com>
2026-09-05 15:21:30 -0600

    Project contacts part 1

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongodb = require('./db/connect');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Connected to DB and listening on ${port}`);
+    });
+  }
+});

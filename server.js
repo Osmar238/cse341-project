fbd500293884e63958c14de2772e3abd17d37ea1
Osmar238 <osmar77770@gmail.com>
2026-09-14 10:24:31 -0600

    Express get

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mongodb = require('./db/connect');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/', require('./routes'));
